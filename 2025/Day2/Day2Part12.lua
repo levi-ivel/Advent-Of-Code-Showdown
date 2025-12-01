d29b53e987d18298c165329086c9fe69bf5f2db4
levi-ivel <levi.kingma@gmail.com>
2025-12-01 20:57:00 +0100

    prep day 2

---
+++ 2025/Day2/Day2Part12.lua
@@ -0,0 +1,31 @@
+function SolvePart2()
+    local counter = 0
+    for line in io.lines('Day2Data.txt') do
+        counter = counter + 1
+    end
+    return counter 
+end
+
+-- Benchmarking
+local average = 0
+
+for i = 0, 1000 do
+    local timestart = os.clock()
+    
+    print(SolvePart2())
+    print()
+    
+    local time = os.clock() - timestart
+    average = average + time
+    print(time)
+    print()
+end
+
+average = average / 1000
+local seconds = average
+local milliseconds = seconds * 1000
+local microseconds = milliseconds * 1000
+
+print("AoC 2025 Day 1 Part 1")
+print(string.format("De average is: %f seconden, (of %f milliseconden), (of %f microseconden)", 
+    seconds, milliseconds, microseconds))
