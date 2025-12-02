079664db2feac823d067e6db8ed6f795bdcb5ce2
levi-ivel <levi.kingma@gmail.com>
2025-12-02 19:17:49 +0100

    Feat: Day 2 Part 1 Solved

--- 2025/Day2/Day2Part1.lua
+++ 2025/Day2/Day2Part1.lua
@@ -1,24 +1,63 @@
 function SolvePart1()
     local counter = 0
-    for line in io.lines('Day2Data.txt') do
-        counter = counter + 1
+    local input = io.open('Day2Data.txt')
+
+    if input then
+        local line = input:read()
+        if line then
+            -- Separate by comma
+            for field in line:gmatch('([^,]+)') do
+                -- Get first and last number of each range
+                local first, last = field:match("(%d+)%-(%d+)")
+                first = tonumber(first)
+                last = tonumber(last)
+
+                local lastDigit = #tostring(last)
+
+                -- Shoutout to https://www.quora.com/What-is-the-sum-of-all-numbers-between-two-given-numbers and https://stackoverflow.com/questions/16344284/how-to-generate-a-list-of-palindrome-numbers-within-a-given-range and https://www.geeksforgeeks.org/dsa/generate-palindromic-numbers-less-n/ and https://www.geeksforgeeks.org/dsa/sum-first-k-even-length-palindrome-numbers/
+
+                -- Using only even digits
+                for digit = 1, math.floor(lastDigit / 2) do
+                    -- Make a base that mirrors numbers of that digit count
+                    local base = 10^digit + 1
+
+                    -- Get the min and max valuess that can form palindromes and fit in the range
+                    local minValue = math.max(math.ceil(first / base), 10^(digit-1))
+                    local maxValue = math.min(math.floor(last / base), 10^digit - 1)
+
+                    -- If a valid range is found
+                    if minValue <= maxValue then
+                        -- Get the amount of numbers in the range
+                        local count = maxValue - minValue + 1
+
+                        -- Calculate the sum of al numbers in tge range
+                        local sum = (minValue + maxValue) * count / 2
+
+                        -- Multiply by the base to get the total sum of the palindromes
+                        local total = base * sum
+
+                        counter = counter + total
+                    end
+                end
+            end
+        end
     end
-    return counter 
+    input:close()
+    return counter
 end
 
 -- Benchmarking
 local average = 0
 
-for i = 0, 1000 do
+for i = 1, 1000 do
     local timestart = os.clock()
     
-    print(SolvePart1())
+    print(SolvePart1()) -- 12586854255
     print()
     
     local time = os.clock() - timestart
     average = average + time
-    print(time)
-    print()
+    print(time * 1000000)
 end
 
 average = average / 1000
